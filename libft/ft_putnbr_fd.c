87896ded8465ea2e897e607daea5daf2d8ef7f43
Edgarrei10 <edgar_fred15@hotmail.com>
2025-04-08 22:19:07 +0100

    Tests and funcs done

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: edfreder <edfreder@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 22:22:38 by edfreder          #+#    #+#             */
-/*   Updated: 2025/04/05 22:22:44 by edfreder         ###   ########.fr       */
+/*   Updated: 2025/04/08 22:10:28 by edfreder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*nbr;
+	long int	n_long;
 
-	nbr = ft_itoa(n);
-	if (!nbr)
-		return ;
-	ft_putstr_fd(nbr, fd);
-	free(nbr);
+	n_long = n;
+	if (n_long < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n_long *= -1;
+	}
+	if (n_long > 9)
+		ft_putnbr_fd(n_long / 10, fd);
+	ft_putchar_fd(n_long % 10 + '0', fd);
 }
